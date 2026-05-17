6b9326d818f6118a5bcb565e24c9945eaaf0ca37
GlacierFroZzt <112581241+GlacierFroZzt@users.noreply.github.com>
2026-05-17 14:42:39 -0600

    Update inicio.html.php

--- Serviclimas/Vistas/inicio.html.php
+++ Serviclimas/Vistas/inicio.html.php
@@ -6,7 +6,7 @@
     <title>Serviclimas</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="../raiz_publica/estilos_premium.css">
+<link rel="stylesheet" href="/estilos_premium.css">
 
    <style>
     /* ==========================================================================
